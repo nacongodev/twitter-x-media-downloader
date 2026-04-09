718984d0aa0e70ae987ecd9bb477dc9da678fa37
Jean-Luc Tsheta <tshieta@gmail.com>
2026-04-09 19:43:35 +0200

    Add default PORT=8000 fallback in start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ cd backend
 if [ -f /opt/venv/bin/activate ]; then
     source /opt/venv/bin/activate
 fi
+# Default PORT to 8000 if not set
+PORT=${PORT:-8000}
 exec python3 -m uvicorn main:app --host 0.0.0.0 --port $PORT
